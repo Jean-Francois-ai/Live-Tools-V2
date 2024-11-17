f37a78bed767697f3da787d6ea0ce20ff321d75d
Jean-Francois-ai <57141777+Jean-Francois-ai@users.noreply.github.com>
2024-11-17 15:42:10 +0100

    Update secret.py

--- secret.py
+++ secret.py
@@ -4,6 +4,7 @@ ACCOUNTS = {
         "secret_api": "6bf881933b7193ccd2265164a6d41d09b0f7261cf1e39cf0809ba9857339b40c",
         "password": "jflb1978bollinger",
     },
+}
 
 #"bitget_exemple": {
 #"apiKey":"bg_631338278f9701a23725445657d4bffa",
