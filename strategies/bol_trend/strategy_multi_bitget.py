d78e57cdf49234a29bce9beb0f3e9aaab0fd1686
Jean-Francois-ai <57141777+Jean-Francois-ai@users.noreply.github.com>
2024-11-17 15:58:02 +0100

    Update strategy_multi_bitget.py

--- strategies/bol_trend/strategy_multi_bitget.py
+++ strategies/bol_trend/strategy_multi_bitget.py
@@ -292,8 +292,8 @@ def close_short(row):
 print(f"--- Bollinger Trend on {len(params_coin)} tokens {timeframe} Leverage x{leverage} ---")
 
 bitget = PerpBitget(
-        public_api=account["public_api"],
-        secret_api=account["secret_api"],
+        public_api=account["publicapi"],
+        secret_api=account["secretapi"],
         password=account["password"],
 )
 
